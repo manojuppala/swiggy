bc8bb10da11b378eee0c057044a994d97450bfc9
manoj uppala <uppala0863@gmail.com>
2022-07-18 02:42:17 +0530

    minor change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV) {
   app.use(express.static(path.join(__dirname, "client/build")));
 }
 // ROUTES //
