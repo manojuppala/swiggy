781d1b46652bb52c4bcff7df66da0bbfab45385e
manoj uppala <uppala0863@gmail.com>
2022-07-18 01:18:41 +0530

    updated build path in production

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  // app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.resolve(process.cwd(), "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), "client/build/index.html"));
+  });
 }
 // ROUTES //
 
