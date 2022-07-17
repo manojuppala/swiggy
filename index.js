538aaa2da893fac348b77b3e479626d9d06cdfcd
manoj uppala <uppala0863@gmail.com>
2022-07-17 23:48:00 +0530

    updated index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "client/build")));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
 // ROUTES //
 
 app.post("/register", async (req, res) => {
